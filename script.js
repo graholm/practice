7d39f9657409f0d1c961253ca9026a172be090eb
graholm <graholm@nuevaschool.org>
2016-10-10 09:54:22 -0700

    Classadoo autocommit

--- script.js
+++
@@ -1,7 +0,0 @@
-var quantityInput = $(".element-quantity-input")
-
-quantityInput.keydown(function(e) {
- var keyCode = e.keyCode
-  var inputValue = quantityInput.val();
-  console.log (keyCode)
-})
